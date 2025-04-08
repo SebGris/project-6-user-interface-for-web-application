483e4d8ebf47d16bf2a5dcaaf0847ac6c0bb2064
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-04-08 12:27:22 +0200

    Ajout de la fonction pour charger dynamiquement les catégories depuis l'API et mise à jour de la liste déroulante dans la modale.

--- script.js
+++ script.js
@@ -107,6 +107,41 @@ async function loadTopRatedMovies(url) {
     }
 }
 
+// Fonction pour charger les catégories depuis l'API
+async function loadCategories(url) {
+    try {
+        // Sélectionner la liste déroulante
+        const categorySelect = document.getElementById('other-categories');
+
+        // Effacer les options existantes
+        categorySelect.innerHTML = '';
+
+        // Charger toutes les pages de catégories
+        while (url) {
+            // Récupérer les données de l'API
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP : ${response.status}`);
+            }
+            const data = await response.json();
+            categories = data.results; // Stocker les catégories
+
+            // Ajouter les nouvelles catégories
+            categories.forEach(category => {
+                const option = document.createElement('option');
+                option.value = category.name.toLowerCase(); // Utiliser le nom comme valeur
+                option.textContent = category.name; // Afficher le nom dans la liste
+                categorySelect.appendChild(option);
+            });
+
+            // Passer à la page suivante (si disponible)
+            url = data.next;
+        }
+    } catch (error) {
+        console.error('Erreur lors du chargement des catégories :', error);
+    }
+}
+
 // Charger le meilleur film au chargement de la page
 document.addEventListener('DOMContentLoaded', async () => {
     const topRatedMoviesUrl = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score';
@@ -120,4 +155,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const movieDetailsUrl = `http://localhost:8000/api/v1/titles/${firstMovieId}`;
         loadBestMovie(movieDetailsUrl);
     }
+    // Charger les catégories au chargement de la page
+    const genresUrl = 'http://localhost:8000/api/v1/genres/';
+    loadCategories(genresUrl);
 });
